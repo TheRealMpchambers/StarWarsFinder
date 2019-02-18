79e0bde82b1f2063e8b8716b163cf7354701905b
TheRealMpchambers <mikkel.p.chambers@gmail.com>
2019-02-18 17:49:31 -0600

    picture link fix

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,31 +1,31 @@
 const friends = [
     {
         name: 'Darth Revan',
-        photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.propsfoundry.com%2Fwp-content%2Fuploads%2F2015%2F05%2Fdarth_revan_sith_mask_kotor_01.jpg&imgrefurl=https%3A%2F%2Fwww.propsfoundry.com%2Fproduct%2Fdarth-revan-mask%2F&docid=wOisE6yO7-4umM&tbnid=fg6VqFpLmp0qcM%3A&vet=10ahUKEwiKqfaLmsbgAhUI4oMKHR2TDbMQMwhvKAQwBA..i&w=1148&h=644&bih=937&biw=1920&q=darth%20revan&ved=0ahUKEwiKqfaLmsbgAhUI4oMKHR2TDbMQMwhvKAQwBA&iact=mrc&uact=8',
+        photo: 'https://cdn3.movieweb.com/i/article/IHAig79xMJGx985W8gFYok3AkP14or/798:50/Last-Jedi-Darth-Revan-Official-Star-Wars-Canon.jpg',
         scores: ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1']
     },
 
     {
         name: 'Darth Maul',
-        photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fstarwarsblog.starwars.com%2Fwp-content%2Fuploads%2F2015%2F07%2FEP1_IA_72118-copy-1536x864-433723767381.jpg&imgrefurl=https%3A%2F%2Fwww.starwars.com%2Fnews%2Fdarth-maul-the-story-of-an-apprentice&docid=bLiIlM7SOqkRVM&tbnid=F1El7AyAuCWHGM%3A&vet=10ahUKEwiip_ykmsbgAhVM4oMKHfvhBH4QMwh4KA0wDQ..i&w=1536&h=864&bih=927&biw=958&q=darth%20maul&ved=0ahUKEwiip_ykmsbgAhVM4oMKHfvhBH4QMwh4KA0wDQ&iact=mrc&uact=8',
+        photo: 'https://www.cheatsheet.com/wp-content/uploads/2016/11/maul.jpg',
         scores: ['3', '3', '3', '3', '3', '3', '3', '3', '3', '3']
     },
 
     {
         name: 'Darth Malgus',
-        photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn3.dualshockers.com%2Fwp-content%2Fuploads%2F2012%2F03%2FDarth-Malgus-7-ds1-1340x1340.jpg&imgrefurl=https%3A%2F%2Fwww.dualshockers.com%2Flife-size-figure-of-darth-malgus-from-star-wars-the-old-republic-for-sale%2F&docid=e8QX1m2tepd6KM&tbnid=X52SlV6tbbn_HM%3A&vet=10ahUKEwjopd-wmsbgAhUptIMKHWmRDHwQMwhQKBAwEA..i&w=1280&h=720&bih=927&biw=958&q=darth%20malgus&ved=0ahUKEwjopd-wmsbgAhUptIMKHWmRDHwQMwhQKBAwEA&iact=mrc&uact=8',
+        photo: 'https://external-preview.redd.it/tk_vbTrnnosaAKvs0ZtNAKX-m3rjn6al0xQfJ5PApw0.jpg?width=960&crop=smart&auto=webp&s=23579657a0d8a5b517775b6f6069e6e375a9b06b',
         scores: ['4', '3', '2', '3', '5', '3', '1', '1', '4', '2']
     },
 
     {
         name: 'Darth Vader',
-        photo: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimagesvc.timeincapp.com%2Fv3%2Ffan%2Fimage%3Furl%3Dhttps%253A%252F%252Fdorksideoftheforce.com%252Ffiles%252F2018%252F06%252Fdarthvader-rogueone.jpg%26c%3Dsc%26w%3D736%26h%3D485&imgrefurl=https%3A%2F%2Fdorksideoftheforce.com%2F2018%2F12%2F20%2Fstar-wars-theorys-vader-fan-film%2F&docid=CoqKF3ordsqNfM&tbnid=9csammUBmtARgM%3A&vet=10ahUKEwjUvr29msbgAhVi7IMKHZ4cDeEQMwiVASgeMB4..i&w=735&h=485&bih=927&biw=958&q=darth%20vader&ved=0ahUKEwjUvr29msbgAhVi7IMKHZ4cDeEQMwiVASgeMB4&iact=mrc&uact=8',
+        photo: 'https://cdn1us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2016/12/darth_vader_primary.jpg?itok=yUofre0m',
         scores: ['1', '2', '3', '4', '5', '5', '4', '3', '2', '1']
     },
 
     {
         name: 'Darth Bane',
-        photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjzq_bOmsbgAhUi6oMKHZjTCAoQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F265923552978270024%2F&psig=AOvVaw3KkzDfc1JU-bM2pBTgCRi7&ust=1550611094191171',
+        photo: 'https://pbs.twimg.com/media/CeFZvdQW4AEPxml.jpg',
         scores: ['5', '5', '5', '5', '5', '5', '5', '5', '5', '5']
     }
 ];
